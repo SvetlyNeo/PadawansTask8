438c98d604e1930b6700bc735eaa7adeaf960560
SvetlyNeo <svetlyneo@gmail.com>
2019-02-22 15:05:04 +0300

    Update WordsManipulation.cs

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -7,8 +7,28 @@ namespace PadawansTask8
     {
         public static void RemoveDuplicateWords(ref string text)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (text.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            StringBuilder sb = new StringBuilder();
+            var words = text.Split(new[] { ' ', '.', ',', '!', '?', '-', ':', ';' });
+            foreach (var word in words)
+            {
+                if (sb.ToString().Contains(word))
+                {
+                    sb.Append(" ");
+                }
+                else
+                {
+                    sb.Append(word.Trim() + " ");
+                }
+            }
+            text = sb.ToString().Substring(0, sb.Length - 1);
         }
     }
 }
